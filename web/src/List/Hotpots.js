f68fb011bc4b6f4468ef12934051571f17378f53
tpw284 <tpw284@gmail.com>
2021-08-09 00:16:15 +0700

    image update

--- web/src/List/Hotpots.js
+++ web/src/List/Hotpots.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { List, Datagrid, TextField, DateField, DeleteButton, EditButton } from 'react-admin';
 import PostPagination from "../Pagination/PostPagination";
+import logo from "../assets/139k.jpg"
 
 export const HotpotList = (props) => (
     <List {...props} title="List of hotpots" >
         <Datagrid>
             <TextField source="name" label="Name" />
+            <img src={logo} style={{height: "70px"}}></img>
             <TextField source="ingredients" label="Ingredients"/>
             <EditButton basePath="./hotpots"/>
             <DeleteButton undoable={false}/>
