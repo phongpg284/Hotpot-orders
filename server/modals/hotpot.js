e29e82fbd8dd1478beec26f8d1232f6f98ac5a90
tpw284 <tpw284@gmail.com>
2021-08-09 00:52:52 +0700

    update image hotpot

--- server/modals/hotpot.js
+++ server/modals/hotpot.js
@@ -4,9 +4,10 @@ const mongoosePaginate = require('mongoose-paginate-v2');
 var hotpotSchema = new mongoose.Schema({
     name: {type: String, require: true},
     ingredients: [{
-            id: Number,
-            name: String,
-        }], 
+        id: Number,
+        name: String,
+    }], 
+    imgUrl: String
 })
 
 hotpotSchema.method("toJSON", function() {
--- web/src/List/Hotpots.js
+++ web/src/List/Hotpots.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { List, Datagrid, TextField, DateField, DeleteButton, EditButton } from 'react-admin';
+import { List, Datagrid, TextField, DateField, DeleteButton, EditButton, ImageField } from 'react-admin';
 import PostPagination from "../Pagination/PostPagination";
 import logo from "../assets/139k.jpg"
 
@@ -7,7 +7,7 @@ export const HotpotList = (props) => (
     <List {...props} title="List of hotpots" >
         <Datagrid>
             <TextField source="name" label="Name" />
-            <img src={logo} style={{height: "70px"}}></img>
+            <ImageField source="imgUrl" />
             <TextField source="ingredients" label="Ingredients"/>
             <EditButton basePath="./hotpots"/>
             <DeleteButton undoable={false}/>
