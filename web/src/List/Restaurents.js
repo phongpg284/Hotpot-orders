eb42b4e6764ce5ca7cf19e5c76f776ce1adbde73
Phong Phi Gia <tpw284@gmail.com>
2021-10-13 22:47:28 +0700

    update

--- web/src/List/Restaurents.js
+++ web/src/List/Restaurents.js
@@ -1,17 +1,21 @@
 import * as React from "react";
-import { List, Datagrid, TextField, DateField, DeleteButton, TextInput, EditButton } from 'react-admin';
+import { List, Datagrid, TextField, DateField, DeleteButton, TextInput, EditButton, ImageField } from 'react-admin';
 import PostPagination from "../Pagination/PostPagination";
+import useStyles from "../hooks/useStyles";
 
 const restaurentFilters = [
     <TextInput label="Search" source="q" alwaysOn />,
     <TextInput label="Title" source="title" defaultValue="Hello, World!" />,
 ];
 
-export const RestaurentList = (props) => (
+export const RestaurentList = (props) => {
+    const classes = useStyles();
+
+    return (
     <List {...props} title="List of Restaurents" pagination={<PostPagination/>} perPage={5}>
         <Datagrid>
             <TextField source="name" label="Name" />
-            <TextField source="logo" label="Logo"/>
+            <ImageField source="logo" label="Logo" className={classes.imgContainer} />
             <TextField source="slogan" label="Slogan"/>
             <TextField source="address" label="Address"/>
             <TextField source="phone" label="Phone"/>
@@ -20,4 +24,4 @@ export const RestaurentList = (props) => (
             <DeleteButton undoable={false}/>
         </Datagrid>
     </List>
-);
+)};
--- web/src/hooks/useStyles.js
+++ web/src/hooks/useStyles.js
@@ -3,8 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles({
     imgContainer: {
         '& img': {
-            height: 100,
-            width: 100,
+            height: 150,
+            width: 150,
             objectFit: "contain"
         }
     }
